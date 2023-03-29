8e12f86adc9eaabb7373638d48ad50feced152d6
prathmesh babar <102967803+prathmeshbabar1@users.noreply.github.com>
2023-03-29 17:18:49 +0530

    Update home.js

--- frontend/src/home-add/home.js
+++ frontend/src/home-add/home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./home.css"
+import "./home.css";
 const Home = ()=>{
 const [ note,setNote] = useState([]);
 // const [key,setKey] = useState('');
